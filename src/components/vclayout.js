bb60e8d02effb105659bf41e1680e11363dc5f56
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-08-23 21:28:59 -0700

    created smaller navbar display

--- src/components/vclayout.js
+++ src/components/vclayout.js
@@ -3,11 +3,11 @@ import { Link } from 'gatsby'
 import VCNavBar from './vcnavbar'
 
 // styles
-import { pageStyles, headingStyles, paragraphStyles, linkStyles, contactStyle } from './vclayout.module.css'
+import { pageStyles, headingStyles, paragraphStyles, linkStyles, contactStyle, footerStyles } from './vclayout.module.css'
 
 const Header = () => {
   return (
-    <div>
+    <header>
       <div className={headingStyles}>
         <h1>Christopher Dreyer</h1>
         <h3>Professor of Practice, Mechanical Engineering</h3>
@@ -17,14 +17,14 @@ const Header = () => {
         <Link to="/bio">Bio</Link>
         <Link to="/publications">Publications</Link>
       </div>
-    </div>
+    </header>
   )
 }
 
 const Footer = () => {
   return (
     <>
-      <div>
+      <div className={paragraphStyles}>
         <h3>RESEARCH AREAS</h3>
           <ul>
             <li>Laser Induced Breakdown Spectroscopy for material analysis</li>
@@ -36,13 +36,13 @@ const Footer = () => {
             <li>In Situ Automated Rock Thin Section Instrument for Planetary Science</li>
           </ul>
       </div>
-      <div>
+      <div className={paragraphStyles}>
         <h3>PROGRAMS AND RESEARCH CENTERS</h3>
           <a href="https://space.mines.edu/" target="_blank" rel="noreferrer noopener">Space Resources Program</a>
           <br></br>
           <a href="https://space.mines.edu/research" target="_blank" rel="noreferrer noopener">Center for Space Resources</a>
       </div>
-      <div className={paragraphStyles}>
+      <footer className={footerStyles}>
         <h4>Contact</h4>
         General Research Lab 234A
         <br />
@@ -56,7 +56,7 @@ const Footer = () => {
           alt="Gatsby G Logo"
           src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
         />
-      </div>
+      </footer>
     </>
   )
 }
