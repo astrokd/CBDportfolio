126b02633df54bf473183d71e76af912199b892a
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-09-03 12:53:47 -0700

    add some styling to header nav and some transitions

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 // styles
-import { headingStyles, linkStyles, } from './vclayout.module.css'
+import { headingStyles, linkStyles, aLinks } from './vclayout.module.css'
 
 const Header = () => {
     return (
@@ -11,11 +11,11 @@ const Header = () => {
           <h1> Christopher Dreyer</h1>
           <h3>Professor of Practice, Mechanical Engineering</h3>
         </div>
-        <div className={linkStyles}>
-          <Link to="/">Home</Link>
-          <Link to="/bio">Bio</Link>
-          <Link to="/publications">Publications</Link>
-        </div>
+        <nav className={linkStyles}>
+          <Link className={aLinks} id="home" to="/">Home</Link>
+          <Link className={aLinks} id="bio" to="/bio">Bio</Link>
+          <Link className={aLinks} id="pub" to="/publications">Publications</Link>
+        </nav>
       </header>
     )
   }
