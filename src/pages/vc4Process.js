13a48c3b7378c186899cc7c42510621f82f884ee
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-09-09 12:11:11 -0700

    added content to the process page

--- src/pages/vc4Process.js
+++ src/pages/vc4Process.js
@@ -10,14 +10,11 @@ const Process = () => {
     <VCLayout activelink="Process">
         <div className={contentStyle}>
             <h2 className={linkSpan}>Process</h2>
-
-            <p>
-            Acre-breadth angstrom barleycorn candela carat clove dalton decibel em fathom fifth gal grain henry hobbet jiffy kilobase kip minim osmol palm pica pitch rod rutherford svedberg tesla tod tog tun. Barn baud carat coulomb denier dyne hertz hundredweight joule kilobase kip langley link osmol pole rod rood stack therm. Abampere arpent astronomical unit cusec footlambert magneton mecate morgan oersted photon pipe rundlet scruple stone therblig. Barn dalton darcy digit dol face-cord fresnel hundredweight peck quarter quire rutherford wey zolotnik. Ampere angstrom baud clove coulomb dram hobbet horsepower jansky jiffy kilderkin magneton mnemon mutchkin pennyweight point poise pole shake yardland. Aam cubit decibel furlong hide langley link mil oersted palm shake siemens ton watt wey yardland.
-            </p>
-
-            <h3>Seed Good Fruitful Creepeth</h3>
-            <h4>Deep Abundantly Upon Created Subdue Life Living</h4>
-            <p>Darkness it creepeth first let land. Were itself, made air lesser divide bearing blessed night fill. Have dry day Seas.</p>
+            <p> Process in the Space Resources Value Chain is the production of goods in space using space resources.  This can include parts, spacecraft, fuel, structures, roads, human consumables, and more. Remember that space resources are more than just material resources. Microgravity, vacuum, photons, for example, are also resources that might be used for manufacturing.</p>
+            <ul>
+              <li><a href="https://space.mines.edu/prof-greg-jackson-to-collaborate-on-nasa-project-with-oxeon-energy/" target="_blank" rel="noreferrer noopener">Tipping Point</a></li>
+              <li><a href="https://space.mines.edu/mines-senior-design-team-selected-as-finalist-for-nasa-3d-printed-habitat-challenge/" target="_blank" rel="noreferrer noopener">NASA 3D Printed Habitat Challenge</a></li>
+            </ul>
         </div>
     </VCLayout>
   )
