9d56d80549ac61374ca6d53bc974de898dd12c89
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-09-08 21:45:17 -0700

    added content to index and explore pages

--- src/pages/vc1Explore.js
+++ src/pages/vc1Explore.js
@@ -10,14 +10,12 @@ const Explore = () => {
     <VCLayout activelink="Explore">
         <div className={contentStyle}>
             <h2 className={linkSpan}>Explore</h2>
-
-            <p>
-            Acre-breadth angstrom barleycorn candela carat clove dalton decibel em fathom fifth gal grain henry hobbet jiffy kilobase kip minim osmol palm pica pitch rod rutherford svedberg tesla tod tog tun. Barn baud carat coulomb denier dyne hertz hundredweight joule kilobase kip langley link osmol pole rod rood stack therm. Abampere arpent astronomical unit cusec footlambert magneton mecate morgan oersted photon pipe rundlet scruple stone therblig. Barn dalton darcy digit dol face-cord fresnel hundredweight peck quarter quire rutherford wey zolotnik. Ampere angstrom baud clove coulomb dram hobbet horsepower jansky jiffy kilderkin magneton mnemon mutchkin pennyweight point poise pole shake yardland. Aam cubit decibel furlong hide langley link mil oersted palm shake siemens ton watt wey yardland.
-            </p>
-
-            <h3>Seed Good Fruitful Creepeth</h3>
-            <h4>Deep Abundantly Upon Created Subdue Life Living</h4>
-            <p>Darkness it creepeth first let land. Were itself, made air lesser divide bearing blessed night fill. Have dry day Seas.</p>
+            <p>The Space Resources Value Chain begins with exploration, this involves finding resources and designing tools to find resources.</p>
+            <ul>
+              <li><a href="https://blog.maxar.com/space-infrastructure/2019/maxar-extends-its-robotic-leadership-to-the-moon?utm_source=maxar.com-robotics-servicing&utm_medium=website" target="_blank" rel="noreferrer noopener">SAMPLR</a></li>
+              <li><a href="https://space.mines.edu/curtis-purrington-wins-nasa-nstgro-fellowship-and-challenge-prize/" target="_blank" rel="noreferrer noopener">ELVIS</a></li>
+              <li><a href="https://ascelibrary.org/doi/abs/10.1061/9780784483374.041" target="_blank" rel="noreferrer noopener">Sensing while drilling</a></li>
+            </ul>
         </div>
     </VCLayout>
   )
