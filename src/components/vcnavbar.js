8009b97165b6cbf4e47b18be08282c83e625a5aa
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-09-12 10:59:48 -0700

    added start svg shape and hidding in other vcs

--- src/components/vcnavbar.js
+++ src/components/vcnavbar.js
@@ -2,18 +2,27 @@ import * as React from "react"
 import { Link } from 'gatsby'
 
 // styles 
-import { topnav, navlink, active, linktext, EFlinktext, ex } from './vcnavbar.module.css'
+import { topnav, navlink, active, linktext, EFlinktext, ex, startsvg, arrowsvg, activelinesvg } from './vcnavbar.module.css'
 
 const Chevron = ({ children, linkpath, linkstyle, abbr, linktext, idname }) => {
   const polywidth = 50
   const polywidthedge = polywidth-15
-  const polypoints = `0,0 15,25 0,50 ${polywidthedge},50 ${polywidth},25 ${polywidthedge},0` 
+  const arrowpolypoints = `0,0 15,25 0,50 ${polywidthedge},50 ${polywidth},25 ${polywidthedge},0` 
+  const startpolypoints = `0,0 0,50 ${polywidthedge},50 ${polywidth},25 ${polywidthedge},0` 
   return (
     <Link className={linkstyle} to={linkpath} id={idname}>
       <p className={linktext}>{ children }</p>
-      <svg viewBox="0 0 50 50" preserveAspectRatio="none">
+      <svg className={activelinesvg} viewBox="0 0 15 50" preserveAspectRatio="none">
+        <polyline points="0,0 15,25 0,50" stroke-width="3" />
+      </svg>
+      <svg className={arrowsvg} viewBox="0 0 50 50" preserveAspectRatio="none">
         {/* <polyline points="0,0 15,25 0,50" /> */}
-        <polygon points={polypoints} />
+        <polygon points={arrowpolypoints} />
+        <text x="55%" y="60%" textAnchor="middle">{ abbr }</text>
+      </svg>
+      <svg className={startsvg} viewBox="0 0 50 50" preserveAspectRatio="none">
+        {/* <polyline points="0,0 15,25 0,50" /> */}
+        <polygon points={startpolypoints} />
         <text x="55%" y="60%" textAnchor="middle">{ abbr }</text>
       </svg>
     </Link>
