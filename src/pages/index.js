6ef20642d8df8673765696d939e5e644d03fadf7
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-09-24 13:10:55 -0700

    pulled html from query and dangerouslysetinnerhtml on main component of index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,48 @@
 import * as React from "react"
 import VCLayout from '../components/vclayout'
-// import { graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 // styles
 import { contentStyle, paragraphStyles } from './vc.module.css'
 
-const ResearchAreas = () => {
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      nodes {
+        html
+        frontmatter {
+          title
+      }
+    }
+  }
+  }`
+
+  
+
+const ResearchAreas = ({ children }) => {
+  // const htmlli = data.allMarkdownRemark.nodes
   return (
     <div className={paragraphStyles}>
         <h3>RESEARCH AREAS</h3>
           <ul>
-            <li>Laser Induced Breakdown Spectroscopy for material analysis</li>
-            <li>Raman spectroscopy for combustion analysis</li>
-            <li>Maritime atmospheric characterization using laser scattering</li>
-            <li>Spectroscopy of Ionic Liquids</li>
-            <li>Surface probe for asteroid characterization</li>
-            <li>Hollow Waveguide Cavity Ringdown Spectroscopy</li>
-            <li>In Situ Automated Rock Thin Section Instrument for Planetary Science</li>
+            { children }
           </ul>
       </div>
   )
 }
 
 // markup
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const html = data.allMarkdownRemark.nodes[0].html
+  const htmlli = data.allMarkdownRemark.nodes[1].html
   return (
     <>
     <VCLayout activelink="Home">
       <p className={contentStyle}>
-        This Portfolio is based around the Space Resources Value Chain.
-        <br></br> 
-        <br></br> 
-        Value chains are used by business and industries to explain what they do. Here are ways to write the value chains for the terrestrial mining industry and the terrestrial manufacturing industry.  
-        <br></br>
-        <br></br>
-        The Space Resources Value Chain begins with Explore(EX), followed by Establish Feasibility(EF), Mine(MN), Process(PC), Manufacture(MF), Distribution(DS), Sell(SL).
-        Select a category of the Space Resources Value Chain above to see Dr. Dreyer’s related work.
+        <div dangerouslySetInnerHTML={{ __html: html }} />
       </p>
       <br></br>
-      <ResearchAreas></ResearchAreas>
+      <ResearchAreas><div dangerouslySetInnerHTML={{ __html: htmlli }} /></ResearchAreas>
     </VCLayout>
     </>
   )
