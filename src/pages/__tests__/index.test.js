3f5fb51073a06b09acc44cef6632da3fca974c2b
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-12-05 17:54:45 -0800

    index test attempt, need to figureout render

--- src/pages/__tests__/index.test.js
+++ src/pages/__tests__/index.test.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { render, screen } from "@testing-library/react"
 import { StaticQuery } from "gatsby" // mocked
-
+import '@testing-library/jest-dom'
 import Index from "../index"
 
 
@@ -11,6 +11,19 @@ beforeEach(() => {
       site: {
         siteMetadata: {
           title: `GatsbyJS`,
+        },
+      },
+    })
+  ).mockImplementationOnce(({ render }) =>
+    render({
+      placeholderImage: {
+        childImageSharp: {
+          fluid: {
+            aspectRatio: 1,
+            sizes: `100 200 300`,
+            src: `pretend-i-am-a-base64-encoded-image`,
+            srcSet: `asdfasdf`,
+          },
         },
       },
     })
@@ -26,4 +39,14 @@ describe('true is truthy and false is falsy', () => {
     expect(false).toBe(false);
   });
 });
+
+// describe(`Index`, () => {
+//   it(`contains this text`, () => {
+//     const { getByText } = render(<Index />)
+
+//     const greeting = getByText(`This Portfolio is based around`)
+
+//     expect(greeting).toBeInTheDocument()
+//   })
+// })
 
